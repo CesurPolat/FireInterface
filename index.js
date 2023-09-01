2e663e901f8e209d75389d79c322a481e3947cc5
CesurPolat <cesurpolat12@gmail.com>
2023-09-01 11:27:40 +0300

    sendTransaction

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 "use strict";
+import { ethers } from "ethers";
+
+var webSocket;
+webSocket = new WebSocket("ws://localhost:5418");
+webSocket.onerror = function (error) {
+  window.open("firewallet://");
+  reject("Err");
+};
+
 function requestAccounts() {
-    var webSocket;
-    return new Promise((resolve, reject) => {
-        webSocket = new WebSocket("ws://localhost:5418");
-        webSocket.onerror = function (error) {
-            window.open("firewallet://")
-            reject("Err")
-        };
-        webSocket.onmessage = async (event) => {
-            resolve(await new Response(event.data).text())
-        };
-        webSocket.onopen = (event) => {
-            webSocket.send('{"method":"requestAccounts"}')
-        };
-    })
+  return new Promise((resolve, reject) => {
+    webSocket.onmessage = async (event) => {
+      resolve(await new Response(event.data).text());
+    };
+    webSocket.onopen = (event) => {
+      webSocket.send('{"method":"requestAccounts"}');
+    };
+  });
 }
 
-module.exports = {requestAccounts}
+function sendTransaction(obj) {
+  return new Promise((resolve, reject) => {
+    webSocket.onmessage = async (event) => {
+        console.log(event.data);
+        resolve(await new Response(event.data).text());
+      };
+      webSocket.onopen = (event) => {
+        webSocket.send(obj);
+      };
+  });
+}
+
+module.exports = { requestAccounts, sendTransaction, ethers };
